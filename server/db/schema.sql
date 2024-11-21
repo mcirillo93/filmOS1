55df48f7147c5b07269dddb55ffd12ca9d49b0dc
mcirillo93 <michael.cirillo007@gmail.com>
2024-11-21 18:03:32 -0500

    package json adjustments

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,4 +1,6 @@
+DROP DATABASE IF EXISTS moviedb;
 CREATE DATABASE moviedb;
+
 
 \c moviedb;
 
@@ -9,14 +11,4 @@ CREATE TABLE movies (
   release_date DATE,
   overview TEXT
 );
-const getMovies = async (): Promise<Movie[]> => {
-  const result = await pool.query<Movie>('SELECT * FROM movies');
-  return result.rows;
-};
-const createMovie = async (movie: Movie): Promise<Movie> => {
-  const result = await pool.query<Movie>(
-    'INSERT INTO movies (title, poster_path, release_date, overview) VALUES ($1, $2, $3, $4) RETURNING *',
-    [movie.title, movie.poster_path, movie.release_date, movie.overview]
-  );
-  return result.rows[0];
-};
+
